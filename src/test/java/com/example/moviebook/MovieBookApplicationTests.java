bb8a7a6741fdaf6016503c65cd0afd374e7af19b
KhamzaKhakim <hamzir.04@gmail.com>
2023-01-09 14:51:26 +0600

    initial commit

---
+++ src/test/java/com/example/moviebook/MovieBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
